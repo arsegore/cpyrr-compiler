d1184055d17a3f39a300807cb765107dcc63ac15
Paul Rodrigues <paul.rodrigues@etu.univ-st-etienne.fr>
2025-10-29 12:38:04 +0100

    modifications table decla

--- src/tab_decla.c
+++ src/tab_decla.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "../inc/tab_decla.h"
+#include "../inc/tab_lexico.h"
 
 /* a faire
    - chainage
@@ -10,13 +11,15 @@
    
 */
 
+int i_deb = DEBORDEMENT; // indice 1ere case LIBRE de la zone de debordement
+
 // mise a -1 /!\ attention penser si des valeurs negatives peuvent exister
 // tout definir a -1 ou juste juqu'a n ? 
-void initialiser(table_decl table, int n){
+void initialiser(table_decl table){
     int i, j;
 
-    for(i = 0; i < n; i++){
-        for(j = 0; j < 5; j++){   
+    for(i = 0; i < HAUTEUR; i++){
+        for(j = 0; j < LARGEUR; j++){   
             table[i][j] = -1;
         }
     }
@@ -24,17 +27,24 @@ void initialiser(table_decl table, int n){
 
 // affiche tous les champs d'une ligne passé en parametre
 void modifier_champ(table_decl table, int ligne, int colonne, int val){
+
+    // Cas d'erreur
+    if (ligne >= HAUTEUR || colonne >= LARGEUR){
+        printf("Erreur, tentative de modifier un champ non defini\n");
+        return;
+    }
+    
     table[ligne][colonne] = val;
 }
 
 void afficher_element(table_decl table, int ligne){
     int i;
     // En-tête du tableau
-    printf("%-3s | %-8s | %-8s | %-8s | %-12s | %-10s |\n",
+    printf("%-4s | %-8s | %-8s | %-8s | %-12s | %-10s |\n",
            "ID", "nature", "suivant", "region", "description", "execution");
     printf("------------------------------------------------------------------\n");
 
-    printf("%-3d | ",ligne);
+    printf("%-4d | ",ligne);
     for(i = 0; i < 3; i++){
         printf("%-8d | ",table[ligne][i]);
     }
@@ -43,31 +53,59 @@ void afficher_element(table_decl table, int ligne){
     printf("\n");
 }
     
-void affichage(table_decl table, int n){
-    int i,j;
+void affichage(table_decl table){
+    int i=0 ,j;
 
     // En-tête du tableau
-    printf("%-3s | %-8s | %-8s | %-8s | %-12s | %-10s |\n",
+    printf("%-4s | %-8s | %-8s | %-8s | %-12s | %-10s |\n",
            "ID", "nature", "suivant", "region", "description", "execution");
     printf("------------------------------------------------------------------\n");
 
-    
-    for(i = 0; i < n; i++){
-        printf("%-3d | ", i);
+    // Affichage table des déclarations
+    while (table[i][0] != -1){
+        printf("%-4d | ", i);
         for(j = 0; j < 3; j++){
             printf("%-8d | ",table[i][j]);
         }
         printf("%-12d | ",table[i][3]);
         printf("%-10d | ",table[i][4]);
         printf("\n");
+        i++;
+    }
+
+    // Affichage zone de debordement
+    printf("-------------------------Zone de debordement----------------------\n");
+    i = DEBORDEMENT;
+    while (table[i][0] != -1){
+        printf("%-4d | ", i);
+        for(j = 0; j < 3; j++){
+            printf("%-8d | ",table[i][j]);
+        }
+        printf("%-12d | ",table[i][3]);
+        printf("%-10d | ",table[i][4]);
+        printf("\n");
+        i++;
     }
 }
 
 // prends la nature en entrée, le numero de region
-void ajouter_element(table_decl table, int nature, int region){
+void ajouter_element(table_decl table, char *lexeme, int nature, int region){
     int i;
 
-    // on cherche l'incide juste après le dernier ajouter
+    // on verifie si c'est la 1ere declaration du lexeme
+    i = recuperer_num_lexico(lexeme); // recup_num_lex() a definir
+
+    // si 1ere declaration on declare dans la case correspondant au num lex sinon on déclare dans la 1ere case de la zone de debordement
+    if (table[i][0] != -1){
+        // on parcours la chaine des suivants jusqu'a trouver la derniere declaration
+        while (table[i][1] != -1){
+            i = table[i][1];
+        }
+        table[i][1] = i_deb; // le suivant du dernier element de la chaine devient l'indice de la nouvelle declaration
+        i = i_deb;
+    }
+    
+    // on cherche l'indice juste après le dernier ajoute
     while(table[i][0] != -1){
         i++;
     }
@@ -79,5 +117,32 @@ void ajouter_element(table_decl table, int nature, int region){
     table[i][4] = -1; // ppir l'instant aucune idee de lexec
     
 }
+    
+int main(){
+    table_decl t;
+
+    initialiser(t);
+    init_tab_lexico();
+
+    inserer_lexeme("a");
+    inserer_lexeme("b");
+    inserer_lexeme("c");
+    
+    ajouter_element(t, "a", N_TAB, 1);
+    ajouter_element(t, "b", N_TAB, 2);
+    ajouter_element(t, "c", N_TAB, 3);
+    ajouter_element(t, "c", N_TAB, 4);
+    ajouter_element(t, "a", N_TAB, 5);
+    ajouter_element(t, "b", N_TAB, 6);
+    ajouter_element(t, "c", N_TAB, 7);
+    ajouter_element(t, "a", N_TAB, 8);
+
+    affichage(t);
 
     
+
+    
+    
+    
+    return 1;
+}
