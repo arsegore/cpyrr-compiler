3b8d8010c637cb894261ffd7639839c561d824b6
Adam <arsegore@ikmail.com>
2025-11-08 12:58:48 +0100

    ajout note

--- src/association_noms/association_noms.c
+++ src/association_noms/association_noms.c
@@ -1,9 +1,12 @@
-#include "../../inc/association_noms/association_noms.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "../../inc/tables/tab_decla.h"
-#include "../../inc/tables/tab_lexico.h"
+#include "association_noms/association_noms.h"
+#include "tables/tab_decla.h"
+#include "tables/tab_lexico.h"
+
+// NOTE : il faut aussi se servir de la pile des régions, voir notes tp
+#include "tables/pile_regions.h"
 
 /*
 Probleme : si on imagine 2 variable x déclaree comme des INT, on recuperera toujours la premiere alors qu'on voulait peut-etre la 2eme
