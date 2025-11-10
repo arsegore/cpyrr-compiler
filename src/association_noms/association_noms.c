6f0e52ceaf52ab922cc2c7e3206e60aae8387585
Paul Rodrigues <paul.rodrigues@etu.univ-st-etienne.fr>
2025-11-10 14:34:44 +0100

    association_noms à test, probleme avec les chemins

--- src/association_noms/association_noms.c
+++ src/association_noms/association_noms.c
@@ -4,18 +4,36 @@
 
 #include "../../inc/tables/tab_decla.h"
 #include "../../inc/tables/tab_lexico.h"
+#include "../../inc/tables/pile_regions.h"
 
 /*
 Probleme : si on imagine 2 variable x déclaree comme des INT, on recuperera toujours la premiere alors qu'on voulait peut-etre la 2eme
 Solution? : parem en + pour différencier?
 Est-ce vraiment un probleme?
 */
+
 int association_noms(int num_lex, int nature){
-    int i = num_lex;
-    
-    while (table_decl[i][NATURE] != nature){
-        i = table_decl[i][SUIVANT];
+    int i = num_lex, /* pour parcourir la table des decla */
+        j = 0, /* servira a simuler le depilement */
+        asso_ok = 0, /* pour savoir si j'ai bien trouve l'association */
+        s; /* le sommet de pile */
+
+
+    while (asso_ok == 0){
+
+        /* je regarde le sommet de pile, j permet de simuler le depilement */
+        s = pile_regions[nb_regions_pile - j++];
+
+        /* une fois le sommet identifie je parcours la table des declarations en utilisant le num lexico jusqu'a trouver la bonne declaration */
+        while (tab_decla[i][REGION] != s){
+            i = tab_decla[i][SUIVANT];
+        }
+
+        /* je verifie que la nature de la decla correspond a la nature recherchee */
+        if (tab_decla[i][NATURE] == nature){
+            asso_ok = 1;
+        }
     }
-    
+
     return i;
 }
