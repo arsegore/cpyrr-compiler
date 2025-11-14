2c40c78b9fd2fdd0b292fb90102205f4e79a3fb9
Paul Rodrigues <paul.rodrigues@etu.univ-st-etienne.fr>
2025-11-14 11:05:28 +0100

    association-nom.c

--- src/association_noms/association_noms.c
+++ src/association_noms/association_noms.c
@@ -20,19 +20,22 @@ Est-ce vraiment un probleme?
 
 int association_noms(int num_lex, int nature){
     int i = num_lex,    /* pour parcourir la table des decla */
-        j = 0,          /* servira a simuler le depilement */
+        j = 1,          /* servira a simuler le depilement */
         asso_ok = 0,    /* pour savoir si j'ai bien trouve l'association */
         s;              /* le sommet de pile */
 
 
-    while (asso_ok == 0){
+    while (asso_ok == 0 && j <= nb_regions_pile){
 
         /* je regarde le sommet de pile, j permet de simuler le depilement */
         s = pile_regions[nb_regions_pile - j++];
+        printf("Sommet de pile = %d\n", s);
 
         /* une fois le sommet identifie je parcours la table des declarations en utilisant le num lexico jusqu'a trouver la bonne declaration */
-        while (tab_decla[i][REGION] != s){
+        while (tab_decla[i][REGION] != s && tab_decla[i][SUIVANT] != -1){
+            printf("   val region pour i = %d : %d\n", i, tab_decla[i][REGION]);
             i = tab_decla[i][SUIVANT];
+            printf("   suivant =  %d\n", tab_decla[i][SUIVANT]);
         }
 
         /* je verifie que la nature de la decla correspond a la nature recherchee */
