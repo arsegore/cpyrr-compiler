bb7024413494c8a6af01eb3bcecf8df3c997af01
DamienGrjn <damien@LAPTOP-N04F54UM.localdomain>
2025-11-21 08:41:55 +0100

    Ajout table_exec, fix ifndef et commentaire

---
+++ inc/arbre/pile_exec.h
@@ -0,0 +1,48 @@
+#ifndef _PILE_EXEC_H_
+#define _PILE_EXEC_H_
+
+/**
+ * pile_exec.h
+ *
+ * Définition de la pile d'exécution et de ses fonctions
+ *
+ * Auteur : GRANJON Damien
+ */
+
+#define TAILLE_PILE_EXEC 5000
+
+extern int pile_exec[TAILLE_PILE_EXEC];
+
+extern int base_courante;
+
+/**
+ * Initialise la pile d'exécution
+ */
+void init_pile_exec();
+
+/**
+ * @return Le sommet de la pile d'exécution
+ */
+int sommet_pile_exec();
+
+/**
+ * 
+ */
+void empiler_pile_exec();
+
+/**
+ * Depile la pile d'exécution
+ */
+void depiler_pile_exec();
+
+/**
+ * Met à jour la variable globale base_courante
+ */
+void maj_base_courante();
+
+/**
+ * Affiche la pile d'exécution
+ */
+void afficher_pile_exec();
+
+#endif
---
+++ src/arbre/pile_exec.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "arbre/pile_exec.h"
+
+int pile_exec[5000];
+
+int base_courante;
+
+void init_pile_exec() {
+
+}
+
+int sommet_pile_exec() {
+    
+}
+
+void empiler_pile_exec() {
+    
+}
+
+void depiler_pile_exec() {
+    
+}
+
+void maj_base_courante() {
+    
+}
+
+void afficher_pile_exec() {
+    
+}
